8bdff61a36c3112be37c31359e936d5a563713f6
ibuki-kamiza <shangjingyichui@gmail.com>
2025-11-03 02:29:35 -0800

    Initial Commit

---
+++ StudyTimerTests/StudyTimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StudyTimerTests.swift
+//  StudyTimerTests
+//
+//  Created by pc on 2025/11/03.
+//
+
+import Testing
+
+struct StudyTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
